1927a428dc6178a67b789439622e259d5dab5213
isaac-kg <isaackagiso7@gmail.com>
2023-05-04 21:18:25 +0200

    about section

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,7 +1,32 @@
+import React from "react";
+import "./about.css"
 const About = props => {
-    return (
-        <h1>About.</h1>
-    )
+  return (
+    <div className="about">
+      <div className="about__info">
+        <div className="about__info-header">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus eum reprehenderit quia?
+        </div>
+        <div className="about__info-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, quaerat! Suscipit voluptatum,
+           saepe eum sapiente dolore ut nesciunt rerum omnis repellat voluptatem?
+        </div>
+        <div className="about__info-service">
+          <div className="about__service">
+            <span className="about__service-header">242</span>
+            <span>Lorem, ipsum<br/> dolor.</span>
+          </div>
+          <div className="about__service">
+            <span className="about__service-header">182</span>
+            <span>Lorem,<br/> ipsum.</span>
+          </div>
+        </div>
+      </div>
+      <div className="about__images">
+        .fafdas
+      </div>
+    </div>
+  )
 }
 
 export default About;
