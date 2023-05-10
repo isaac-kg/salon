e7d2e769ab5a189339457b6ac198d05761ba8d52
isaac-kg <isaackagiso7@gmail.com>
2023-05-10 21:04:22 +0200

    skeleton

--- src/components/productBooking/index.jsx
+++ src/components/productBooking/index.jsx
@@ -1,5 +1,17 @@
 const ProductBooking = props => {
+  return (
+    <div className="product-booking">
+      <div className="product-booking__image">
 
+      </div>
+      <div className="product-booking__title">
+        Lorem ipsum dolor sit.
+      </div>
+      <div className="product-booking__description">
+
+      </div>
+    </div>
+  )
 }
 
 export default ProductBooking;
---
+++ src/components/productOverview/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ProductOverview = () => {
+  return (
+    <div className="product-overview">
+      <div className="product-overview__title">
+        Lorem ipsum dolor sit.
+      </div>
+      <div className="product-overview__sub-heading">
+        Lorem ipsum dolor sit amet consectetur.
+      </div>
+      <div className="product-overview__description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Similique placeat quis optio eos pariatur libero aliquam nulla consectetur,
+        deleniti maxime? Delectus facilis blanditiis repudiandae.
+      </div>
+      <div className="product-overview__btn">
+        Lorem, ipsum.
+      </div>
+    </div>
+  )
+}
