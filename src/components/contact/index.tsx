1c0bc6f4568063ff677743d8e9d08117a0e82795
isaac-kg <isaackagiso7@gmail.com>
2023-06-16 21:27:59 +0200

    contact

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,12 +1,14 @@
-const Contact = (props) => {
+import "./style.css"
+const Contact = () => {
   return <div className="contact">
     <div className="contact__title">
       Subscribe to our newsletter
     </div>
     <div className="contact__email">
-      
+      <input type="text" placeholder="your email..."/>
     </div>
+    <div className="contact__button">Subscribe</div>
   </div>
 }
 
-export default Contact
+export default Contact;
