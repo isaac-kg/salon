776a1c9a1de9c001ec8c20286520a184730d409c
isaac-kg <isaackagiso7@gmail.com>
2023-06-22 21:12:46 +0200

    added bookings

--- src/components/bookings/index.tsx
+++ src/components/bookings/index.tsx
@@ -1,5 +1,19 @@
+import "./style.css";
+
 const Booking = () => {
-  return <div>This is booking</div>
+  return (<div className="booking">
+    <img src="src/assets/cosmetics.jpg" className="booking__image" />
+
+    <div className="booking__card">
+      <div className="booking__title">BOOK TIME TO GET MANICURE</div>
+      <div className="booking__description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Voluptatum quibusdam saepe cum aspernatur, iure mollitia laboriosam. 
+        Ipsam sit officia dicta officiis placeat culpa, eveniet unde autem suscipit pariatur.
+      </div>
+      <button className="booking__button">Booking</button>
+    </div>
+  </div>)
 }
 
 export default Booking;
