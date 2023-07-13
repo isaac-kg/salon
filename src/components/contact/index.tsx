e6eb839e84e14727cf99fe432ea1c25d0435f55a
isaac-kg <isaackagiso7@gmail.com>
2023-07-13 21:03:43 +0200

    contact

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -2,7 +2,7 @@ import "./style.css"
 const Contact = () => {
   return <div className="contact">
     <div className="contact__title">
-      Subscribe to our newsletter
+      Subscribe to our<br/> newsletter
     </div>
     <div className="contact__email">
       <input type="text" placeholder="your email..."/>
