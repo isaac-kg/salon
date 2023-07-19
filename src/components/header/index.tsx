68e35f8a86951c070653f224227b1b68daacf792
isaac-kg <isaackagiso7@gmail.com>
2023-07-19 21:58:47 +0200

    header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,21 @@
 import React from "react"
+import "./style.css"
 
 const Header = (props: any) => {
   return <div className="header">
     <div className="header__title">Lorem, ipsum dolor.</div>
-    <div className="header__subtitle">Lorem ipsum dolor sit amet consectetur.</div>
-    <button className="header__button">Read More</button>
+    <div className="header__navigation">
+      <a href="" className="header__links">Home</a>
+      <a href="" className="header__links">About Us</a>
+      <a href="" className="header__links">Product</a>
+      <a href="" className="header__links">Contact Us</a>
+    </div>
+    <div className="header__cal-action">
+      <p className="header__contact">+27 123 142 4422</p>
+      <div>
+        <button className="header__button">Read More</button>
+      </div>
+    </div>
   </div>
 }
 
