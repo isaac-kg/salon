84c9e4b448f8ccfe146dfa913ce0e40949435f93
isaac-kg <isaackagiso7@gmail.com>
2023-11-16 22:04:59 +0200

    header

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,6 @@ function App() {
       {/* 
      http://preview.themeforest.net/item/atura-nail-salon-wordpress-theme/full_screen_preview/35352438?_ga=2.85836569.1048488018.1682965854-301774857.1682965854
        */}
-
-       {/* 
-          do a hero page and a header
-          Have to make the page have all the neccessary components
-          I must make sure every thing is connected and is responsive 
-          I have to add colors and property images.
-          I have  to host the site.
-          I have to start implementing the logic.
-       */}
-       <Header />
        <Hero />
        <About />
 
