9bc5c834911e5e30cb606ebc9034702416d21124
isaac-kg <isaackagiso7@gmail.com>
2024-01-07 09:44:57 +0200

    hero updated

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,10 +3,12 @@ import "./style.css"
 
 const Hero = (props: any) => {
   return <div className="hero">
-    <div className="hero__title">Reveal Your Inner Glow at Radiance Haven!</div>
+    <div className="hero__title">
+      Reveal Your Inner Glow at Radiance Haven!
+      </div>
     <div className="hero__subtitle">
-    Are you ready to embrace a new chapter of beauty and<br /> 
-    self-discovery? We invites you to experience a world where every<br /> moment is crafted to nurture your unique radiance.
+      Are you ready to embrace a new chapter of beauty and<br /> 
+      self-discovery? We invites you to experience a world where every<br /> moment is crafted to nurture your unique radiance.
     </div>
     <div>
 
