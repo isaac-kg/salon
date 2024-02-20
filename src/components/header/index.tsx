438c93e185b613f8037a23d3aa99b0de171f72d3
isaac-kg <isaackagiso7@gmail.com>
2024-02-20 20:16:22 +0200

    links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,25 @@
 import React from "react"
 import "./style.css"
-import { useNavigate } from "react-router-dom" 
+import { useNavigate } from "react-router-dom"
 
 const Header = (props: any) => {
-
-  const navigate = useNavigate();
-  return <div className="header">
-    <div className="header__title">Radiance Haven</div>
-    <div className="header__cal-action">
-      <div onClick={() => navigate("product-list")}>
-        <img style={{width: "40px"}} src="../../assets/icons/cart.svg" alt="" />
+  const navigate = useNavigate()
+  return (
+    <div className="header">
+      <div className="header__title" onClick={() => navigate("/")}>
+        Radiance Haven
+      </div>
+      <div className="header__cal-action">
+        <div onClick={() => navigate("product-list")}>
+          <img
+            style={{ width: "40px" }}
+            src="../../assets/icons/cart.svg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
-  </div>
+  )
 }
 
 export default Header
--- src/components/shopNow/index.tsx
+++ src/components/shopNow/index.tsx
@@ -1,37 +1,52 @@
 import "./style.css"
+import { useNavigate } from "react-router-dom"
 
 const ShopNow = () => {
+  const navigate = useNavigate()
   return (
     <div className="shopNow-container">
       <div className="shopNow">
         <div className="shopNow__image">
-          <img className="shopNow__image-1" src="../../assets/makeup.jpg" alt="" />
+          <img
+            className="shopNow__image-1"
+            src="../../assets/makeup.jpg"
+            alt=""
+          />
         </div>
         <div className="shopNow__description">
-          <div className="shopNow__description__title">
-            Makeup
-          </div>
+          <div className="shopNow__description__title">Makeup</div>
           <div className="shopNow__description__info">
-          Are you ready to shine? Dive into our makeup collection and elevate your beauty game! Discover your perfect look today by exploring our collection and finding the ideal match for your style
+            Are you ready to shine? Dive into our makeup collection and elevate
+            your beauty game! Discover your perfect look today by exploring our
+            collection and finding the ideal match for your style
           </div>
-          <button className="shopNow__description__btn">
+          <button
+            className="shopNow__description__btn"
+            onClick={() => navigate("/")}
+          >
             Shop Now
           </button>
         </div>
       </div>
-      
+
       <div className="shopNow">
         <div className="shopNow__image shopNow__image-two">
-          <img className="shopNow__image-1" src="../../assets/nails.jpg" alt="" />
+          <img
+            className="shopNow__image-1"
+            src="../../assets/nails.jpg"
+            alt=""
+          />
         </div>
         <div className="shopNow__description shopNow__description-two">
-          <div className="shopNow__description__title">
-           Manicure
-          </div>
+          <div className="shopNow__description__title">Manicure</div>
           <div className="shopNow__description__info">
-          Get your perfect manicure! Treat your nails to some love and care with our exclusive collection. Shop now and pamper yourself!
+            Get your perfect manicure! Treat your nails to some love and care
+            with our exclusive collection. Shop now and pamper yourself!
           </div>
-          <button className="shopNow__description__btn">
+          <button
+            className="shopNow__description__btn"
+            onClick={() => navigate("/")}
+          >
             Shop Now
           </button>
         </div>
@@ -40,4 +55,4 @@ const ShopNow = () => {
   )
 }
 
-export default ShopNow;
+export default ShopNow
